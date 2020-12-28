e4a9be5fef9287a14d24796c13da1345c424adc9
James Hodgkinson <james@terminaloutcomes.com>
2020-12-29 09:51:07 +1000

    making pylint happy

--- app/download_image_map.py
+++ app/download_image_map.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+""" config file parser """
+
 import csv
 
-
 class Parser():
+    """ config file parser """
     def __init__(self, csv_file=None):
         self._parsed_data = None
         self.csv_file = csv_file
@@ -10,12 +12,14 @@ class Parser():
 
     @property
     def parsed_data(self):
+        """ return the parsed data """
         if self._parsed_data is not None:
             return self._parsed_data
         self._parsed_data = self.get_data()
         return self._parsed_data
 
     def transform_csv_data(self, data):
+        """ transform the parsed data """
         transformed = {}
         key = ""
         row_counter = 0
@@ -24,21 +28,25 @@ class Parser():
                 row_counter = row_counter + 1
                 key = line[0]
                 transformed[key] = line[1]
-            except IndexError as e:
-                print("userdata.csv line {}: Malformed or Missing fields in data '{}'. Error: '{}'".format(row_counter, line, e))
+            except IndexError as error_message:
+                # pylint: disable=line-too-long
+                print(f"userdata.csv line {row_counter}: Malformed or Missing fields in data '{line}'. Error: '{error_message}'")
         return transformed
 
     def import_csv(self):
-        with open(self.csv_file) as f:
-            csvreader = csv.reader(f, delimiter='|')
+        """ import the csv file """
+        with open(self.csv_file) as csv_file_handle:
+            csvreader = csv.reader(csv_file_handle, delimiter='|')
             data = list(csvreader)
         return data
 
     def get_data(self):
+        """ output the transformed data """
         data = self.import_csv()
         return self.transform_csv_data(data)
 
     def reload(self):
+        """ reload the configuration """
         self._parsed_data = None
         self.get_data()
         return self.parsed_data
