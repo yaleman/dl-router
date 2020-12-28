49fb46d72a5df562e4b9a8f157f3ce860bf4b47b
James Hodgkinson <james@terminaloutcomes.com>
2020-12-29 09:51:25 +1000

    adding some checks so docker doesn't blat files

--- run_local_dev.sh
+++ run_local_dev.sh
@@ -10,6 +10,29 @@ LISTEN_PORT=5000
 CONTAINER_NAME=redirect
 DETACH=false
 
+if [ ! -d "${APP_PATH}" ]; then
+    echo "Unable to find app dir at ${APP_PATH}, quitting"
+    exit 1
+fi
+
+if [ ! -f "${USERDATA_PATH}" ]; then
+    echo "Unable to find userdata.csv at ${USERDATA_PATH}, quitting"
+    exit 1
+fi
+
+if [ ! -f "${MIRRORS_CONF_PATH}" ]; then
+    echo "Unable to find mirrors.yaml at ${MIRRORS_CONF_PATH}, quitting"
+    exit 1
+fi
+
 ##FIXME CHANGE CONFIG MAP TO YAML WHEN DONE
 
-sudo docker run --rm $([[ ${DETACH} == "true" ]] && echo "-d") -v ${APP_PATH}:/app -v ${USERDATA_PATH}:/app/userdata.csv -v ${MIRRORS_CONF_PATH}:/app/mirrors.yaml -p ${LISTEN_PORT}:80 --name ${CONTAINER_NAME} quay.io/lanefu/nginx-uwsgi-flask:arm64
+sudo docker run \
+    --rm \
+    $([[ ${DETACH} == "true" ]] && echo "-d") \
+    -v "${APP_PATH}:/app" \
+    -v "${USERDATA_PATH}:/app/userdata.csv" \
+    -v "${MIRRORS_CONF_PATH}:/app/mirrors.yaml" \
+    -p "${LISTEN_PORT}:80" \
+    --name "${CONTAINER_NAME}" \
+    quay.io/lanefu/nginx-uwsgi-flask:arm64
